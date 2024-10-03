60a78833e7ca453179f7316bb368577478adc704
chaitanya bagade <chaitanyabagade59@gmail.com>
2024-10-03 14:44:27 +0530

    change the login css

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -41,15 +41,15 @@ const Login = () => {
     <div className='loginpage'>
        <div className="loginBox mt-[100px] ml-[2%] min-w-[350px] w-[600px] p-5 ">
        <h1>Create User</h1>
-          <label className='flex  font-bold text-4xl'><img className="w-[60px] h-auto pr-2" src={userimg} alt="" />User Name</label><br/>
-          <input  className='flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="Enter your name..." type="text"name="name" value={name} onChange={(e) => setName(e.target.value)}/><br/>
-          <label className="flex  font-bold text-4xl" htmlFor=''><img  className="w-[50px] h-auto pr-2" src={mobileimg} /> Mobile No.</label><br/>
-          <input  className='flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="ex. 1234567890"type="text"name="mobile"value={mobile}onChange={(e) => setMobile(e.target.value)}/><br/>
-          <label className="flex  font-bold text-4xl"htmlFor=''><img className="w-[50px] h-auto pr-2" src={passimg} alt=""/>Admin Pass</label><br/>
-          <input  className='flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="Admin Password..."type="password" value={adminPass}onChange={(e) => setAdminPass(e.target.value)}/><br/>
-          <label className="flex  font-bold text-4xl" htmlFor=''><img className="w-[50px] h-auto pr-2" src={passimg} alt=""/>Password</label><br/>
-          <input  className='flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="Admin Password..."type="password"name="pass"value={pass}onChange={(e) => setPass(e.target.value)}/><br/>
-          <button  className="text-4xl text-center w-[44%] ml-[28%] rounded-full bg-blue-600"type="submit" onClick={handleSubmit}>Submit</button>
+          <label className='flex  font-bold text-4xl'><img className="w-[60px] h-auto pr-2" src={userimg} alt="" />User Name</label>
+          <input  className=' mt-2 flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="Enter your name..." type="text"name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+          <label className="  mt-2 flex  font-bold text-4xl" htmlFor=''><img  className="w-[50px] h-auto pr-2" src={mobileimg} /> Mobile No.</label>
+          <input  className='  flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="ex. 1234567890"type="text"name="mobile"value={mobile}onChange={(e) => setMobile(e.target.value)}/>
+          <label className=" mt-2 flex  font-bold text-4xl"htmlFor=''><img className="w-[50px] h-auto pr-2" src={passimg} alt=""/>Admin Pass</label>
+          <input  className='flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="Admin Password..."type="password" value={adminPass}onChange={(e) => setAdminPass(e.target.value)}/>
+          <label className=" mt-2 flex  font-bold text-4xl" htmlFor=''><img className="w-[50px] h-auto pr-2" src={passimg} alt=""/>Password</label>
+          <input  className='flex font-bold text-3xl rounded-full pl-2 mr-2 w-[95%]' placeholder="Admin Password..."type="password"name="pass"value={pass}onChange={(e) => setPass(e.target.value)}/>
+          <button  className=" mt-3 text-4xl text-center w-[44%] ml-[28%] rounded-full bg-blue-600"type="submit" onClick={handleSubmit}>Submit</button>
          
 
        </div>
