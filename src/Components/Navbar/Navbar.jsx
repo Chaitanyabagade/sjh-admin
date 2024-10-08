66ea2af2a4c58905d2de9357ecfd743a6f10bf4e
chaitanya bagade <chaitanyabagade59@gmail.com>
2024-10-08 19:46:15 +0530

    change some in Footer.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,94 +9,94 @@ const Navbar = () => {
   const [style11, setStyle11] = useState("firstline");
   const [style12, setStyle12] = useState("Secondline");
   const [style13, setStyle13] = useState("thirdline");
- 
+
   const handleClick = () => {
-      if (style){
-        setStyle(0);
-        setStyle11("firstline");
-        setStyle12("Secondline");
-        setStyle13("thirdline");
-      }
-      else{ 
-        setStyle(1);
-        setStyle11("firstlineCros");
-        setStyle12("SecondlineCros");
-        setStyle13("thirdlineCros");
-      
-      }
+    if (style) {
+      setStyle(0);
+      setStyle11("firstline");
+      setStyle12("Secondline");
+      setStyle13("thirdline");
+    }
+    else {
+      setStyle(1);
+      setStyle11("firstlineCros");
+      setStyle12("SecondlineCros");
+      setStyle13("thirdlineCros");
+
+    }
   };
-  
-  const [auth,setAuth]=useState("");
-  const [reload,seteload]=useState("")
-  var reload_flag=localStorage.getItem('reload_flag');
-  if(reload_flag!=null){
+
+  const [auth, setAuth] = useState("");
+  const [reload, seteload] = useState("")
+  var reload_flag = localStorage.getItem('reload_flag');
+  if (reload_flag != null) {
     localStorage.removeItem('reload_flag');
     window.location.reload();
   }
- 
 
-  let navigate =useNavigate();
-  useEffect(()=>{
-    
-    var auth=localStorage.getItem('user_name');
-    if(auth===null){
-       navigate('/');
-       
+
+  let navigate = useNavigate();
+  useEffect(() => {
+
+    var auth = localStorage.getItem('user_name');
+    if (auth === null) {
+      navigate('/');
+
     }
-    else{
+    else {
       setAuth(auth);
     }
-    
-  },[])
-  
-  
-    
-   
+
+  }, [])
+
+
+
+
   return (
 
-    
-    <div className='NavBox justify-between '>
-      
-      <button onClick={handleClick} className="sideBarButton">
-          <div className={ style11}></div>
-          <div className={ style12}></div>
-          <div className={ style13}></div>
-      </button>
-    
-    
-      
-     
-    
-      <div>
-      {!(auth) ? (
-         <div className={!style?`w-fit h-fit bg-black fixed ml-[-200px]`:`w-fit h-fit bg-black fixed` }>
-         <li className="do-not-delet w-[180px] h-0"></li>
-       
-        <Link  to="/">  <li style={{width:'150px'}}>Admin Page</li></Link>
-      <Link  to="about"> <li>About</li></Link> 
-        <Link  to="login"> <li>Login</li></Link> 
-      <Link  to="contact"> <li>Contact</li></Link> 
-         </div>
-      ) : (
-        <div className={!style?`w-fit h-fit bg-black fixed ml-[-200px]`:`w-fit h-fit bg-black fixed` }>
-        <li className="do-not-delet w-[180px] h-0"></li>
-        <Link to="dashboard"><li style={{color:'orange',width:'180px'}}>Hi.{localStorage.getItem('team')}</li></Link> 
-       
-        <Link  to="sign_up"> <li  style={{width:'180px'}}> Create user</li></Link> 
-       <Link to="deposites"> <li>Deposites</li></Link> 
-       <Link to="loans"> <li>Loans</li></Link> 
-       
-       <Link to="penaltys"> <li>Penaltys</li></Link>
-       <Link  to="expendatures"> <li>Expendature</li></Link>
-       <a  href="logout"><li>Logout</li></a>  
-        </div>
-      )}
-   
-    </div>
 
-    
+    <div className='NavBox bg-black fixed justify-center z-10 '>
+
+      <button onClick={handleClick} className="sideBarButton">
+        <div className={style11}></div>
+        <div className={style12}></div>
+        <div className={style13}></div>
+      </button>
+
+
+
+      <div>
+        {!(auth) ? (
+          <div className={!style ? `w-fit h-fit ml-[-200px]` : `w-fit h-fit bg-black fixed`}>
+            <li className="do-not-delet w-[180px] h-0"></li>
+            <Link to="/">  <li style={{ width: '150px' }}>Admin Page</li></Link>
+            <Link to="about"> <li>About</li></Link>
+            <Link to="login"> <li>Login</li></Link>
+            <Link to="contact"> <li>Contact</li></Link>
+          
+          </div>
+           
+        ) : (
+          <div className={!style ? `w-fit h-fit ml-[-200px]` : `w-fit h-fit bg-black fixed`}>
+            <li className="do-not-delet w-[180px] h-0"></li>
+            <Link to="dashboard"><li style={{ color: 'orange', width: '180px' }}>Hi.{localStorage.getItem('team')}</li></Link>
+            <Link to="sign_up"> <li style={{ width: '180px' }}> Create user</li></Link>
+            <Link to="deposites"> <li>Deposites</li></Link>
+            <Link to="loans"> <li>Loans</li></Link>
+            <Link to="penaltys"> <li>Penaltys</li></Link>
+            <Link to="expendatures"> <li>Expendature</li></Link>
+            <a href="logout"><li>Logout</li></a>
+          
+          </div>
+        )}
         
         
+
+
+      </div>
+     
+
+
     </div>
   )
 }
